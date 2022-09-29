984b3b241000b299407757573786d175f734ce21
kilian.jimenez <kilian.jimenez@bitpanda.com>
2022-09-29 10:34:12 +0200

    feat: Port handling. Status code fix.

--- app/src/main/java/project/coict/buyandsell/LoginActivity.java
+++ app/src/main/java/project/coict/buyandsell/LoginActivity.java
@@ -8,20 +8,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.HttpURLConnection;
+
 public class LoginActivity extends AppCompatActivity {
 
-    //final String URL = "http://192.168.0.22:3000/login";
-    final String URL = "http://192.168.0.22:8080/login";
-    final String code = "responseCode";
-    final String description = "description";
+    final String API_PORT = "3000";
+    final String WIREMOCK_PORT = "8080";
+    final String URL = "http://192.168.0.22:" + API_PORT + "/login";
+
+    int statusCode;
 
     Button btnlogin;
     EditText mailField, passField;
@@ -68,21 +73,16 @@ public class LoginActivity extends AppCompatActivity {
                     try {
 
                         JSONObject jsonObject = new JSONObject(response);
-                        int code = jsonObject.getInt(LoginActivity.this.code);
-                        String description = jsonObject.getString(LoginActivity.this.description);
+                        Log.d("Response: ", jsonObject.toString());
 
-                        if(code == 200){ // OK
+                        if(statusCode == HttpURLConnection.HTTP_OK){
 
                             //Call MainActivity if successful login
                             Intent intent = new Intent(LoginActivity.this,MainActivity.class);
                             startActivity(intent);
 
-                        }
-
-                        else if(code == 403)
-                            Toast.makeText(getApplicationContext(), "Error: Invalid credentials", Toast.LENGTH_LONG).show();
-                        else
-                            Toast.makeText(getApplicationContext(), "Error: " + description, Toast.LENGTH_LONG).show();
+                        } else
+                            Toast.makeText(getApplicationContext(), "Error: Something went wrong...", Toast.LENGTH_LONG).show();
 
                     } catch (JSONException e) {
 
@@ -94,10 +94,17 @@ public class LoginActivity extends AppCompatActivity {
                 error -> {
 
                     // Handle error
-                    Toast.makeText(getApplicationContext(),
-                            "Error: " + error,Toast.LENGTH_SHORT).show();
+                    if(error.networkResponse.statusCode == HttpURLConnection.HTTP_FORBIDDEN)
+                        Toast.makeText(getApplicationContext(),
+                                "Error: Invalid credentials", Toast.LENGTH_LONG).show();
+                    else
+                        Toast.makeText(getApplicationContext(),
+                                "Error: " + error,Toast.LENGTH_SHORT).show();
 
-                }){
+                    Log.d("Error: ", error.toString());
+
+                }
+                ){
 
             @Override
             public byte[] getBody() {
@@ -107,6 +114,12 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public String getBodyContentType() {
                 return "application/json; charset=utf-8";
+            }
+
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                statusCode = response.statusCode;
+                return super.parseNetworkResponse(response);
             }
 
             };
